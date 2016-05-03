7b98862273930d5cd0119753a5d21976e801c0c5
jeremyhilliard14 <jeremyhilliard14@gmail.com>
2016-05-03 16:04:08 -0400

    added registration page started options page added session storage added encryption

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var mongoUrl = 'mongodb://localhost:27017/coffee';
 var mongoose = require('mongoose');
 var Account = require('../models/accounts');
+var bcrypt = require('bcrypt-nodejs');
 mongoose.connect(mongoUrl);
 
 /* GET home page. */
@@ -13,20 +14,29 @@ router.get('/', function(req, res, next) {
 
 // GET route for the register page
 router.get('/register', function(req, res, next){
-	res.render("register", {})
+	res.render("register", {failure: req.query.failure})
 });
 
 //POST route for register
 router.post('/register', function(req, res, next){
 	//the user posted: username, email, password, password2
-	var newAccount = new Account({
-		username: req.body.username,
-		password: req.body.password,
-		emailAddress: req.body.emailAddress
-	});
-	newAccount.save();
-	res.json(req.body)
-	//res.render("register", {})
+	if(req.body.password != req.body.password2){
+		res.redirect('/register?failure=password')
+	}else{
+		var newAccount = new Account({
+			username: req.body.username,
+			password: bcrypt.hashSync(req.body.password),
+			emailAddress: req.body.email
+		});
+		newAccount.save();
+		req.session.username = req.body.username;
+		res.redirect('/options')
+		//res.render("register", {})
+	}
+});
+
+router.get('/options', function(req, res, next){
+	res.render('options', {username: req.session.username})
 });
 
 module.exports = router;
